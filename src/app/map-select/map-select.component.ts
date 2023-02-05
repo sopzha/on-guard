d6b97c50fcfc566c65a9961e352d06e628d4847a
SophieZhang <sopszha@gmail.com>
2023-02-04 20:07:23 -0500

    Configure parent child data sharing in select map

--- src/app/map-select/map-select.component.ts
+++ src/app/map-select/map-select.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MapDirectionsService } from '@angular/google-maps';
+import { map, Observable } from 'rxjs';
+
+import { IncidentService } from '../incident.service';
 
 @Component({
   selector: 'app-map-select',
   templateUrl: './map-select.component.html',
   styleUrls: ['./map-select.component.css']
 })
-export class MapSelectComponent {
+export class MapSelectComponent implements OnInit {
 
+  center: google.maps.LatLngLiteral = {lat: 42.3600825, lng: -71.0588801};
+  zoom = 10;
+
+  @Input() origin: string;
+  @Input() destination: string;
+  @Input() travelMode: google.maps.TravelMode;
+
+  incidentPositions: google.maps.LatLngLiteral[] = [];
+  markerClustererImagePath =
+      'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m';
+
+  directionsResults$?: Observable<google.maps.DirectionsResult|undefined>;
+  
+  constructor(private mapDirectionsService: MapDirectionsService,
+              private incidentService: IncidentService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  generateDirections(newDirectionsRequest: google.maps.DirectionsRequest) {
+    this.directionsResults$ = this.mapDirectionsService.route(newDirectionsRequest).pipe(
+      map(response => response.result)
+    );
+  }
+  
+  moveMap(event: google.maps.MapMouseEvent) {
+    this.center = (event.latLng.toJSON());
+  }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
   styleUrls: ['./user-input.component.css']
 })
-export class UserInputComponent {
+export class UserInputComponent implements OnInit {
 
+  @Output() newDirectionsRequestEvent = new EventEmitter<google.maps.DirectionsRequest>();
+  
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(origin: string, destination: string, travelMode: string) {
+    this.newDirectionsRequestEvent.emit({
+      origin: origin,
+      destination: destination,
+      travelMode: travelMode === 'walking' ? google.maps.TravelMode.WALKING :
+                  travelMode === 'driving' ? google.maps.TravelMode.DRIVING : 
+                                             google.maps.TravelMode.BICYCLING
+    });
+  };
 }
